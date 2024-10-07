e573f9df1facf71dedd470c2e3c2ed2f17c08fbe
kjkTommy <vk2218484@gmail.com>
2024-10-07 17:58:18 +0300

    second commit

--- src/request/requestCounters.ts
+++ src/request/requestCounters.ts
@@ -3,7 +3,7 @@ import { CounterResponse } from '../type';
 
 export const fetchCounter = async (offset: number, limit: number): Promise<CounterResponse> => {
   const response = await axios.get(
-    `http://showroom.eis24.me/api/v4/test/meters/?limit=${limit}&offset=${offset}`
+    `https://showroom.eis24.me/api/v4/test/meters/?limit=${limit}&offset=${offset}`
   );
   console.log(response.data);
 
